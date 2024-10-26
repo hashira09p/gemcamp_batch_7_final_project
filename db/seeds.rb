08445b30de4931e0100ee5084a949ffda819b878
jerome <jeromeabelida19@gmail.com>
2024-10-26 20:33:26 +0800

    added: faker gem [Feature 1]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+3.times do |i|
+  begin
+    User.create!(
+      email: Faker::Internet.email,
+      password: "password#{i}", # Devise will encrypt this
+      username: Faker::Internet.username,
+      phone_number: "09326184829",
+      coins: rand(0..1000),
+      total_deposit: Faker::Number.decimal(l_digits: 8, r_digits: 2),
+      children_members: rand(0..5),
+      remember_created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+      role: rand(0..1)
+    )
+    puts "Created user ##{i + 1}"
+  rescue => e
+    puts "Error creating user ##{i + 1}: #{e.message}"
+  end
+end
+
