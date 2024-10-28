ff4d35a2bbbb3cc03d000e2956ee770660b04998
jerome <jeromeabelida19@gmail.com>
2024-10-29 00:31:39 +0800

    added: design login home page [feature 3]

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,22 @@
   )
 end
 
-User.create(
-  email: 'jerome@koda.com',
-  password: "qwerty", # Devise will encrypt this
-  username: Faker::Internet.username,
-  phone_number: "09326184829",
-  coins: rand(0..1000),
-  total_deposit: Faker::Number.decimal(l_digits: 8, r_digits: 2),
-  children_members: rand(0..5),
-  remember_created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-  role: 1
-)
+User.create([{ email: 'jerome@koda.com',
+     password: "qwerty", # Devise will encrypt this
+     username: Faker::Internet.username,
+     phone_number: "09326184829",
+     coins: rand(0..1000),
+     total_deposit: Faker::Number.decimal(l_digits: 8, r_digits: 2),
+     children_members: rand(0..5),
+     remember_created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+     role: 1 },
+      {email: 'jer@koda.com',
+      password: "qwerty", # Devise will encrypt this
+      username: Faker::Internet.username,
+      phone_number: "09326184829",
+      coins: rand(0..1000),
+      total_deposit: Faker::Number.decimal(l_digits: 8, r_digits: 2),
+      children_members: rand(0..5),
+      remember_created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+      role: 0
+}])
