b480bf6844cfc24dd819432617539bd803f1fe08
hashira <jeromeabelida19@gmail.com>
2024-11-05 18:19:02 +0800

    change null into true in username column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_05_064106) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_05_100007) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_05_064106) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "username"
+    t.string "username", default: ""
     t.string "phone_number"
     t.integer "coins", default: 0
     t.decimal "total_deposit", precision: 10, scale: 2, default: "0.0"
