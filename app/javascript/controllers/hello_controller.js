b69b17121e9012fee1f8b8678db67db4d80106f7
hashira <jeromeabelida19@gmail.com>
2024-11-06 19:41:51 +0800

    added: stimulus js [feature 6]

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  static targets = [ "name" ]
+
+
+  greet() {
+    console.log("Hello, Stimulus!", this.element)
+
+    const element = this.nameTarget
+
+    const name = element.value
+         console.log(`Hello, ${name}!`)
   }
 }
---
+++ db/migrate/20241106104203_add_address_to_user.rb
@@ -0,0 +1,4 @@
+class AddAddressToUser < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
