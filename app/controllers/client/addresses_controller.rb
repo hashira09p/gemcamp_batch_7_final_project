dcd30804a575bc6362ec590d8b4dbf4cbfba5d86
hashira <jeromeabelida19@gmail.com>
2024-11-08 10:36:23 +0800

    added: create in address form

--- app/controllers/client/addresses_controller.rb
+++ app/controllers/client/addresses_controller.rb
@@ -1,17 +1,34 @@
 class Client::AddressesController < ApplicationController
-  def index;end
+
+  before_action :find_user_address, only: :index
+  def index
+    @addresses
+  end
 
   def new
     @address = Address.new
   end
 
   def create
-    address = Address.new()
+    address = Address.new(address_params)
+    address.user = current_client_user
+
+    if address.save
+      flash[:notice] = 'Succesfully added'
+      redirect_to addresses_path
+    else
+      flash[:alert] = 'failed to add'
+      redirect_to new_address_path
+    end
   end
 
   private
 
+  def find_user_address
+    @addresses = Address.where(user_id: current_client_user)
+  end
+
   def address_params
-    params.required(:address).permit(:name, :street_number, :phone_number, :remark, :is_default, :current_user, :address_region_id, :address_provinces_id, :address_city_id, :address_barangay_id)
+    params.require(:address).permit(:genre, :name, :street_address, :phone_number, :remark, :is_default, :address_region_id, :address_province_id, :address_city_id, :address_barangay_id)
   end
 end
