68ff7ddb4b6c8eaac35bd14885a24728053c0082
jerome <jeromeabelida19@gmail.com>
2024-11-10 22:13:00 +0800

    modified: create and update method in registrations controller

--- app/controllers/client/registrations_controller.rb
+++ app/controllers/client/registrations_controller.rb
@@ -1,19 +1,19 @@
 class Client::RegistrationsController < Devise::RegistrationsController
+  before_action :store_promoter, only: [:new]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   # GET /resource/sign_up
-  #  def new
-  #    @user_client = User.client.new
-  #    render 'devise/registrations/new'
-  #  end
+   def new
+     super
+   end
 
   # POST /resource
-   def creates
-     super
-     if resource.persisted?
-       flash[:notice] = "Welcome! You have signed up successfully."
-     else
-       flash[:alert] = "Sign-up failed. Please check the errors below and try again."
+   def create
+     super do |user|
+       if cookies[:promoter].present?
+         promoter = User.find_by(email: cookies[:promoter])
+         user.update(parent_id: promoter.id) if promoter
+       end
      end
    end
 
@@ -42,49 +42,32 @@ class Client::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
   def update
-    @client_user = current_client_user  # Assuming current_client_user returns the logged-in user
+    @client_user = current_client_user
 
-    if @client_user.valid_password?(params[:client_user][:current_password]) && params[:client_user][:username].present? && params[:client_user][:image].present?
-      if @client_user.update(username: params[:client_user][:username], image: params[:client_user][:image])
+    if @client_user.valid_password?(params[:client_user][:current_password])
+      update_params = {}
+      update_params[:username] = params[:client_user][:username] if params[:client_user][:username].present?
+      update_params[:image] = params[:client_user][:image] if params[:client_user][:image].present?
+
+      if params[:client_user][:password].present? && params[:client_user][:password] == params[:client_user][:password_confirmation]
+        update_params[:password] = params[:client_user][:password]
+        if @client_user.update(update_params)
+          flash[:notice] = 'Update Success'
+          redirect_to new_client_user_session_path and return
+        end
+      elsif @client_user.update(update_params)
         flash[:notice] = 'Update Success'
-        redirect_to client_root_path
-      else
-        flash[:alert] = @client_user.errors.full_messages.to_sentence
-        redirect_to edit_client_user_registration_path
+        redirect_to client_root_path and return
       end
 
-    elsif  @client_user.valid_password?(params[:client_user][:current_password]) && params[:client_user][:username].present?
-            if @client_user.update(username: params[:client_user][:username], image: params[:client_user][:image])
-              flash[:notice] = 'Update Success'
-              redirect_to client_root_path
-            else
-              flash[:alert] = @client_user.errors.full_messages.to_sentence
-              redirect_to edit_client_user_registration_path
-            end
-
-    elsif  @client_user.valid_password?(params[:client_user][:current_password]) && params[:client_user][:image].present?
-      if @client_user.update(image: params[:client_user][:image])
-        flash[:notice] = 'Update Success'
-        redirect_to client_root_path
-      else
-        flash[:alert] = @client_user.errors.full_messages.to_sentence
-        redirect_to edit_client_user_registration_path
-      end
-
-    elsif params[:client_user][:password] == params[:client_user][:password_confirmation] && @client_user.valid_password?(params[:client_user][:current_password])
-
-      if params[:client_user][:username].present?
-        @client_user.update(username: params[:client_user][:username], password: params[:client_user][:password])
-        redirect_to new_client_user_session_path
-      else
-        @client_user.update(password: params[:client_user][:password])
-        redirect_to new_client_user_session_path
-      end
+      flash[:alert] = @client_user.errors.full_messages.to_sentence
     else
-      flash[:alert] = 'Invalid password or username is blank'
-      redirect_to edit_client_user_registration_path
+      flash[:alert] = 'Invalid password'
     end
+
+    redirect_to edit_client_user_registration_path
   end
+
 
   # DELETE /resource
   # def destroy
@@ -109,7 +92,17 @@ class Client::RegistrationsController < Devise::RegistrationsController
     params.require(:client_user).permit(:email, :password)
   end
 
+  def store_promoter
+    if params[:promoter].present?
+      cookies[:promoter] = params[:promoter]
+    end
+  end
+
   protected
+
+  def after_sign_up_path_for(resource)
+    client_root_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -122,9 +115,7 @@ class Client::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    client_root_path
-  end
+
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
