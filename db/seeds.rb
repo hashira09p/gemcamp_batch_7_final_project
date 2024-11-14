520cb2ef7ba3f0acbcbe27394f3e01900617c2e8
hashira <jeromeabelida19@gmail.com>
2024-11-14 19:22:56 +0800

    removed item create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,33 +31,3 @@ User.create([{ email: 'jerome@koda.com',
                remember_created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
                role: 0
              }])
-
-3.times do
-  Item.create!(
-    image: Faker::Avatar.image,
-    name: Faker::Commerce.product_name,
-    quantity: Faker::Number.between(from: 1, to: 100),
-    minimum_tickets: Faker::Number.between(from: 1, to: 10),
-    state: '', # Randomly pick one of the AASM states
-    batch_count: Faker::Number.between(from: 1, to: 20),
-    online_at: Faker::Time.between(from: 1.year.ago, to: Time.now, format: :default),
-    offline_at: Faker::Time.between(from: Time.now, to: 1.year.from_now, format: :default),
-    start_at: Faker::Time.between(from: Time.now, to: 1.month.from_now, format: :default),
-    status: ['active', 'inactive'].sample
-  )
-end
-
-3.times do
-  Item.create!(
-    image: Faker::Avatar.image,
-    name: Faker::Commerce.product_name,
-    quantity: Faker::Number.between(from: 1, to: 100),
-    minimum_tickets: Faker::Number.between(from: 1, to: 10),
-    state: '', # Randomly pick one of the AASM states
-    batch_count: Faker::Number.between(from: 1, to: 20),
-    online_at: Faker::Time.between(from: 1.year.ago, to: Time.now, format: :default),
-    offline_at: Faker::Time.between(from: Time.now, to: 1.year.from_now, format: :default),
-    start_at: Faker::Time.between(from: Time.now, to: 1.month.from_now, format: :default),
-    status: ['active', 'inactive'].sample
-  )
-end
