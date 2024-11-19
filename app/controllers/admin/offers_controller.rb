f106323d682fbbda2f7fa32559573276341481b3
jerome <jeromeabelida19@gmail.com>
2024-11-19 23:19:50 +0800

    added: update and delete for offer method

--- app/controllers/admin/offers_controller.rb
+++ app/controllers/admin/offers_controller.rb
@@ -1,5 +1,5 @@
 class Admin::OffersController < ApplicationController
-  before_action :offer_params, only: :create
+  before_action :set_offer, only: [:edit, :update, :destroy]
   def index
     @offers = Offer.all
   end
@@ -13,12 +13,36 @@ class Admin::OffersController < ApplicationController
     if @offer.save
       redirect_to offers_path, notice: 'Offer was successfully created.'
     else
-      render :show
+      render :new
       flash[:alert] = "Failed To Add"
     end
   end
 
+  def edit;end
+
+  def update
+    if @offer.update(offer_params)
+      redirect_to offers_path, notice: 'Offer was successfully updated.'
+    else
+      render :edit
+      flash[:alert] = "Failed To update"
+    end
+  end
+
+  def destroy
+    if @offer.destroy
+      redirect_to offers_path, notice: 'Offer was successfully deleted.'
+    else
+      render :edit
+      flash[:alert] = "Failed To delete"
+    end
+  end
+
   private
+
+  def set_offer
+    @offer = Offer.find(params[:id])
+  end
 
   def offer_params
     params.require(:offer).permit(:name, :status, :amount, :coin, :image)
