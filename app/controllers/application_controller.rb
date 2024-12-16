5b3cee4225ad12469d0012ec6ea1a60ea8e67b50
jerome <jeromeabelida19@gmail.com>
2024-12-16 23:44:46 +0800

    added: tagalog translation for client home page [feature 34]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    @params = params[locale: :tg]
-    I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
+    @params = params[locale: :en]
+    I18n.locale = params[:locale] ||  I18n.default_locale
     session[:locale] = I18n.locale if params[:locale]
   end
 end
